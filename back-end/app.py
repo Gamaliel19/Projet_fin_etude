2da111c39c5454540d8ba3823309acbf42c96ed4
Gamaliel19 <yagalinaf@gmail.com>
2023-11-22 16:40:08 +0100

    deuxième commit

--- back-end/app.py
+++ back-end/app.py
@@ -63,30 +63,6 @@ def login_user():
         "Message":"Connexion réussie!"
     })
 
-@app.route('/creerProd', methods=["POST"])
-def creerProd():
-    dci= request.json["dci"]
-    nom= request.json["nom"]
-    dosage = request.json["dosage"]
-    form = request.json['form']
-    lot = request.json["lot"]
-    date_fab= request.json["date_fab"]
-    date_peremp = request.json["date_peremp"]
-    prix_unit = request.json["prix_unit"]
-    qte_en_stock = request.json["qte_en_stock"]
-    
-
-    new_produit = Produit(dci=dci,nom=nom,dosage=dosage,form=form,lot=lot,date_fab=date_fab,date_peremp=date_peremp,prix_unit=prix_unit,qte_en_stock=qte_en_stock)
-    db.session.add(new_produit)
-    db.session.commit()
-
-    return jsonify("Le produit a été ajouté avec succès!")
-
-@app.route('/listProd', methods=['GET'])
-def listProd():
-    listProd = Produit('SELECT * FROM produits').fetchall()
-    return jsonify(listProd)
-
 
 if __name__=="__main__":
     app.run(debug=True)
--- back-end/models.py
+++ back-end/models.py
@@ -13,16 +13,3 @@ class User(db.Model):
     nom = db.Column(db.String(255), nullable=False)
     profil = db.Column(db.String(255), nullable=False)
     password = db.Column(db.Text, nullable=False)
-
-class Produit(db.Model):
-    __tablename__="produits"
-    id = db.Column(db.String(32), primary_key=True, unique=True,default=get_uuid)
-    dci = db.Column(db.String(255))
-    nom = db.Column(db.String(255))
-    dosage = db.Column(db.String(10), nullable=False)
-    form = db.Column(db.String(255))
-    lot = db.Column(db.String(10))
-    date_fab = db.Column(db.String, nullable=False)
-    date_peremp = db.Column(db.String, nullable=False)
-    prix_unit = db.Column(db.Float)
-    qte_en_stock = db.Column(db.Integer)
