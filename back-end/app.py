70883cb09c6c333be983dd08463167fd7928b980
masrayam <oliviermasrayam8@gmail.com>
2023-12-04 09:55:12 +0100

    changement

--- back-end/app.py
+++ back-end/app.py
@@ -4,12 +4,7 @@ from flask_bcrypt import Bcrypt
 from flask_cors import CORS, cross_origin
 from flask_session import Session
 from config import ApplicationConfig
-<<<<<<< HEAD
-from models import db, User, Utilisateur, Product, Client
-db = SQLAlchemy
-=======
 from models import db, User
->>>>>>> fd1759c5480da96d4b553d32cfd22c10b0cd5dc0
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 app= Flask(__name__)
@@ -52,6 +47,67 @@ def register_user():
      "Message": "Utilisateur créé avec succès!"
     })
 
+#liste des utilisateurs
+@app.route('/list', methods=["GET"])
+def list(self):
+    users = User.query.all()
+    liste=[]
+    for user in users:
+        data={}
+        data['id'] = user.id
+        data['nom'] = user.nom
+        data['prenom'] = user.prenom
+        data['password'] = user.password
+        data['profil'] = user.profil
+        liste.append(data)
+    return jsonify({'utilisateur':liste})
+
+#obtenir un seul tilisateur
+@app.route('/singleUser', methods= ['GET'])
+def singleUser(self,id):
+    user = User.query.filter_by(id=id).first()
+    if user:
+        return user.json()
+    return {'message': 'utilisateur non trouve'}
+#supprimer un utilisateur 
+@app.route('/delete_user/<string:id>', methods=['DELETE'])
+def delete_user(self,id):
+    user = User.query.filter_by(id=id).first()
+    if user:
+        db.session.delete(user)
+        db.session.commit()
+        return {'message': 'utilisateur supprime avec succes'}
+    else:
+        return {'message': 'utilisateur non trouve'}
+
+
+#mettre a jour un utilisateur
+@app.route('/update_user/<string:id>', methods=['PUT'])
+def update_user(self,id):
+    data = request.get_json()
+    user  = User.query.get(id)
+    if data['nom']:
+        user.nom = data['nom']
+    if data['prenom']:
+        user.prenom = data['prenom']
+    if data['email']:
+        user.email = data['email']
+    if data['profil']:
+        user.profil = data['profil']
+    db.session.add(user)
+    db.session.commit()
+    return jsonify({'message': 'utilisateur mis a jour avec succes avec succes', 'utilisateur':{'id':utilisateur.id, 'nom':utilisateur.nom, 'prenom':utilisateur.prenom} })
+
+@app.route('/update_password', methods=['PUT'])
+def update_password(self,id):
+    data = request.get_json()
+    user = User.query.get(id)
+    if data['password']:
+        password = request.json["password"]
+        if password != hashed_password:
+            return jsonify({'message': 'veuillez entrer votre mot de passe precedent'})
+    return jsonify({'message': 'mot de passe modifie avec succes'})
+        
 
 @app.route('/login', methods=["POST"])
 def login_user():
--- back-end/models.py
+++ back-end/models.py
@@ -1,20 +1,96 @@
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from uuid import uuid4
 
-db = SQLAlchemy
+db = SQLAlchemy()
 
-def get_uuid():
-    return uuid4().hex
+class User(db.Model):
+    __tablename__='users'
+    id = db.Column(db.Integer, primary_key=True)
+    nom = db.Column(db.String(100), nullable=True)
+    prenom = db.Column(db.String(50))
+    motPasse = db.Column(db.String(50), nullable=True)
+    profil= db.Column(db.String(30))
 
-class Utilisateur(db.Model):
-    __tablename__="users"
-    id = db.Column(db.String(32), primary_key=True, unique=True,default=get_uuid)
-<<<<<<< HEAD
-    nom = db.Column(db.String(255), nullable=False)
-    profil = db.Column(db.String(255), nullable=False)
-=======
-    email = db.Column(db.String(345), unique=True)
->>>>>>> fd1759c5480da96d4b553d32cfd22c10b0cd5dc0
-    password = db.Column(db.Text, nullable=False)
+    utilisateur=db.relationship('Vente', backref='user', lazy=False)
+    utilisateur=db.relationship('Commande', backref='user',lazy=False)
+    utilisateur=db.relationship('Livraison', backref='user', lazy=False)
+    def __init__(self, nom, prenom, motPasse, profil):
+        self.nom = nom
+        self.prenom = prenom
+        self.motPasse = motPasse
+        self.profil = profil
+    def json(self):
+        return {"nom":self.nom, "prenom":self.prenom, "motPasse":self.motPasse, "profil":self.profil}
+class Produit(db.Model):
+    __tablename__='produits'
+    id = db.Column(db.Integer, primary_key=True)
+    dosage = db.Column(db.String(80))
+    nom_com= db.Column(db.String)
+    description = db.Column(db.String)
+    prix = db.Column(db.Integer())
+    date_fab = db.Column(db.Date)
+    date_per=db.Column(db.Date)
+    qte_stock=db.Column(db.Integer)
+    num_Lot = db.Column(db.Integer)
+    produit=db.relationship('Commande', backref='produit', lazy=False)
+    produit=db.relationship('Inventaire', backref='produit', lazy=False)
 
-    
+    def __init__(self, dosage, nom_com, description, prix, date_fab, date_per, qte_stock, num_Lot):
+        self.dosage=dosage
+        self.nom_com=nom_com
+        self.description=description
+        self.prix = prix
+        self.date_fab=date_fab
+        self.date_per=date_per
+        self.qte_stock=qte_stock
+        self.num_Lot=num_Lot
+        
+    def json(self):
+        return {"dosage":self.dosage, "nom_com":self.nom_com, "prix":self.prix, "date_fab":self.date_fab, "date_per":self.date_per, "qte_stock":self.qte_stock, "num_lot":self.num_Lot}
+
+
+class Vente(db.Model):
+    __tablename__='ventes'
+    id=db.Column(db.Integer, primary_key=True)
+    numero=db.Column(db.Integer)
+    date=db.Column(db.Date)
+    utilisateur_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+
+
+
+class Inventaire(db.Model):
+    __tablename__='inventaires'
+    id=db.Column(db.Integer, primary_key=True)
+    libelle=db.Column(db.String)
+    qte_achete=db.Column(db.Float)
+    qte_vendu=db.Column(db.Float)
+    qte_stock=db.Column(db.Integer)
+    prix_achat=db.Column(db.Float)
+    prix_vente=db.Column(db.Float)
+    total_vendu=db.Column(db.Integer)
+    produit_id=db.Column(db.Integer, db.ForeignKey('produits.id'), nullable=False)
+
+
+class Commande(db.Model):
+    __tablename__='commandes'
+    id=db.Column(db.Integer, primary_key=True)
+    numero=db.Column(db.Integer)
+    qte=db.Column(db.Integer)
+    utilisateur_id= db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    prduit_id= db.Column(db.Integer, db.ForeignKey('produits.id'), nullable=False)
+
+class Livraison(db.Model):
+    __tablename__='livraisons'
+    id=db.Column(db.Integer, primary_key=True)
+    numero=db.Column(db.Integer)
+    date=db.Column(db.Date)
+    utilisateur_id= db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+
+class Client(db.Model):
+    __tablename__='clients'
+    id=db.Column(db.String, primary_key=True)
+    nom=db.Column(db.String)
+    prenom=db.Column(db.String)
+    email=db.Column(db.String)
+    password=db.Column(db.String, nullable=False)
+    confirmed_password=db.Column(db.String, nullable=False)
